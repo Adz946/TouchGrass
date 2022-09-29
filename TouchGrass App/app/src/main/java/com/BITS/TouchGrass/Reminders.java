b5f1d1221b1bfb6d1d527ebc895c5c75c31d6495
Adz946 <100402347+Adz946@users.noreply.github.com>
2022-09-29 14:43:29 +1000

    Made an "example" file
    So that we can write out types of Java functions

---
+++ TouchGrass App/app/src/main/java/com/BITS/TouchGrass/Reminders.java
@@ -0,0 +1,8 @@
+package com.BITS.TouchGrass;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Reminders extends AppCompatActivity {
+    public static void main(String[] args[]){
+        System.out.println("AbCdEfG");
+    }
+}
