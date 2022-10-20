86f3b0a3e0dc2dc8576c57a0e0d489def1708bb7
Fi-Boo <s2008156@student.rmit.edu.au>
2022-10-20 11:07:33 +1100

    branching so i can test coding

--- TouchGrass App/app/src/main/java/com/BITS/TouchGrass/profile/ProfileMainFragment.java
+++ TouchGrass App/app/src/main/java/com/BITS/TouchGrass/profile/ProfileMainFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.BITS.TouchGrass.R;
 
@@ -18,7 +20,24 @@ public class ProfileMainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile_main, container, false);
+    }
+
+    public void onLoginBtnClick (View view) {
+
+        TextView txtViewErrorMsg = getView().findViewById(R.id.txtViewErrorMsg);
+
+        EditText editTxtUsername = getView().findViewById(R.id.editTxtUsername);
+        EditText edtTxtPW = getView().findViewById(R.id.edtTxtPW);
+
+        if (editTxtUsername.getText().toString().equalsIgnoreCase("bob")) {
+            txtViewErrorMsg.setText("Success!");
+        } else {
+            txtViewErrorMsg.setText ("Failed!");
+        }
+        txtViewErrorMsg.setText("Username is..." + editTxtUsername.getText().toString());
+
     }
 }
