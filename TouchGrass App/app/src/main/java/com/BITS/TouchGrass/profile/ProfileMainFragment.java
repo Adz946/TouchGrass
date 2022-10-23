d37906d1a19141d12848621266fa7571875d5b1c
Fi-Boo <s2008156@student.rmit.edu.au>
2022-10-23 20:38:49 +1100

    GOT THE LOGIN BUTTON WORKING

--- TouchGrass App/app/src/main/java/com/BITS/TouchGrass/profile/ProfileMainFragment.java
+++ TouchGrass App/app/src/main/java/com/BITS/TouchGrass/profile/ProfileMainFragment.java
@@ -7,12 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.BITS.TouchGrass.R;
 
-public class ProfileMainFragment extends Fragment {
+public class ProfileMainFragment extends Fragment implements View.OnClickListener {
+    Button myButton;
+    View myView;
 
     public ProfileMainFragment(){
         // require a empty public constructor
@@ -20,24 +23,44 @@ public class ProfileMainFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        myView = inflater.inflate(R.layout.fragment_profile_main, container, false);
+        myButton = (Button) myView.findViewById(R.id.btnLogin);
+        myButton.setOnClickListener(this);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_main, container, false);
+        return myView;
+
     }
 
-    public void onLoginBtnClick (View view) {
+//    public void onLoginBtnClick (View view) {
+//
+//        TextView txtViewErrorMsg = getView().findViewById(R.id.txtViewErrorMsg);
+//
+//        EditText editTxtUsername = getView().findViewById(R.id.editTxtUsername);
+//        EditText edtTxtPW = getView().findViewById(R.id.edtTxtRegPW);
+//
+//        if (editTxtUsername.getText().toString().equalsIgnoreCase("bob")) {
+//            txtViewErrorMsg.setText("Success!");
+//        } else {
+//            txtViewErrorMsg.setText ("Failed!");
+//        }
+//        txtViewErrorMsg.setText("Username is..." + editTxtUsername.getText().toString());
+//
+//    }
 
-        TextView txtViewErrorMsg = getView().findViewById(R.id.txtViewErrorMsg);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                TextView txtViewErrorMsg = getView().findViewById(R.id.txtViewErrorMsg);
+                EditText editTxtUsername = getView().findViewById(R.id.editTxtUsername);
+                EditText edtTxtPW = getView().findViewById(R.id.edtTxtRegPW);
 
-        EditText editTxtUsername = getView().findViewById(R.id.editTxtUsername);
-        EditText edtTxtPW = getView().findViewById(R.id.edtTxtRegPW);
-
-        if (editTxtUsername.getText().toString().equalsIgnoreCase("bob")) {
-            txtViewErrorMsg.setText("Success!");
-        } else {
-            txtViewErrorMsg.setText ("Failed!");
+                if (editTxtUsername.getText().toString().equalsIgnoreCase("bob")) {
+                    txtViewErrorMsg.setText("Success!");
+                } else {
+                    txtViewErrorMsg.setText("Failed!");
+                }
+                break;
         }
-        txtViewErrorMsg.setText("Username is..." + editTxtUsername.getText().toString());
-
     }
 }
