924d1d96cf78123cc0c41c0f226afd3a235be6d7
Jay-J-Williams <101154320+Jay-J-Williams@users.noreply.github.com>
2022-11-08 17:44:00 +1100

    Challenge Creation Page Basics

--- TouchGrass App/app/src/main/java/com/BITS/TouchGrass/challenges/ChallengesMainFragment.java
+++ TouchGrass App/app/src/main/java/com/BITS/TouchGrass/challenges/ChallengesMainFragment.java
@@ -8,13 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.BITS.TouchGrass.R;
 
 public class ChallengesMainFragment extends Fragment {
 
     View view;
-    Button CreateChallengeButton, AcceptButton, RejectButton;
+    Button CreateChallengeButton, AcceptButton, RejectButton, Mon, Tue, Wed, Thu, Fri, Sat, Sun;
+    EditText EnterChallengeName;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +26,19 @@ public class ChallengesMainFragment extends Fragment {
         // Create the page here, and methods referring to the built page +assets on it are
         // referenced using 'view.'
 
+        //Buttons
         CreateChallengeButton = (Button) view.findViewById(R.id.btnCreateChallenge);
         AcceptButton = (Button) view.findViewById(R.id.btnAccept);
         RejectButton = (Button) view.findViewById(R.id.btnReject);
+        Mon = (Button) view.findViewById(R.id.btnMon);
+        Tue = (Button) view.findViewById(R.id.btnTue);
+        Wed = (Button) view.findViewById(R.id.btnWed);
+        Thu = (Button) view.findViewById(R.id.btnThu);
+        Fri = (Button) view.findViewById(R.id.btnFri);
+        Sat = (Button) view.findViewById(R.id.btnSat);
+        Sun = (Button) view.findViewById(R.id.btnSun);
+
+        EnterChallengeName = (EditText) view.findViewById(R.id.EnterChallengeName);
         return view;
     }
 }
