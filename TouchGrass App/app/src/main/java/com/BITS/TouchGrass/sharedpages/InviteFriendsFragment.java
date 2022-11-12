61829ac965a94ca6c613cfe4bf0ecdfa794384ac
Gigiphilac <claydiebear@gmail.com>
2022-11-12 17:16:47 +1100

    DONT CHAT TO ME

--- TouchGrass App/app/src/main/java/com/BITS/TouchGrass/sharedpages/InviteFriendsFragment.java
+++ TouchGrass App/app/src/main/java/com/BITS/TouchGrass/sharedpages/InviteFriendsFragment.java
@@ -7,15 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.BITS.TouchGrass.R;
 
 public class InviteFriendsFragment extends Fragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    Button inviteBtn, cancelBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +23,23 @@ public class InviteFriendsFragment extends Fragment {
 
         // Create the page here, and methods referring to the built page +assets on it are
         // referenced using 'view.'
+        initWidgets(view);
+
+        setListeners();
 
         return view;
+    }
+
+
+    private void initWidgets(View view) {
+        inviteBtn = view.findViewById(R.id.invite_friends_button);
+        cancelBtn = view.findViewById(R.id.cancel_button);
+    }
+
+
+    private void setListeners() {
+        cancelBtn.setOnClickListener(v -> {
+            getParentFragmentManager().popBackStack();
+        });
     }
 }
